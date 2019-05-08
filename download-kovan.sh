8001dffb8e674c0c8a7d41bc3d28a10da2fca7e1
phahulin <p.hahulin@hintsolutions.ru>
2019-05-08 10:46:07 +0300

    Check if archive exists. Hide archive download for now

--- download-kovan.sh
+++ download-kovan.sh
@@ -17,11 +17,15 @@ sleep 60
 echo "$(date) Stopping parity"
 systemctl stop poa-parity
 
-echo "$(date) Downloading chains archive"
-curl -sSfLO "https://s3.amazonaws.com/misc-temp-files/kovan-chains.tar.gz"
+if [ -f "kovan-chains.tar.gz" ]; then
+    echo "Chain archive already exist, skipping download"
+else
+    echo "$(date) Downloading chains archive"
+    curl -sSfLO "https://s3.amazonaws.com/misc-temp-files/kovan-chains.tar.gz"
 
-echo "$(date) Checking archive integrity"
-echo "68a6185ad64a19709f2281bad561be94510a1fbc918bde2d291285f9d5c6b365 kovan-chains.tar.gz" | sha256sum -c
+    echo "$(date) Checking archive integrity"
+    echo "7d060c2a539188029b98cde1a5b1d70c73b767c510bc40907ab01bf1230e1fca kovan-chains.tar.gz" | sha256sum -c
+fi
 
 echo "$(date) Removing existing kovan chains data"
 rm -rf chains/kovan
@@ -40,4 +44,12 @@ systemctl restart poa-parity
 sleep 10
 systemctl restart poa-netstats
 
-echo "$(date) Done. Bye-bye"
+echo "$(date) Waiting 60s to check that parity service did not crash"
+sleep 60
+ETIME="$(ps -p $(pidof parity) -o etimes | head -n2 | tail -n1)"
+if [ "$ETIME" -gt 30 ]; then
+    echo "$(date) Service seems to be up and running. Removing chains archive"
+    rm kovan-chains.tar.gz
+else
+    echo "$(date) Service is not running"
+fi
